987f379253ad766f6d2ba306574eb0d75da81116
Asonglefac Millenium Nzefe <asonglefacmillenium@gmail.com>
2020-09-04 14:49:31 +0100

    Using functions to improve the code

--- script code/condition.js
+++ script code/condition.js
@@ -1,10 +1,14 @@
-var age = prompt("Enter your age ");
+function checkDriverAge() {
+    var age = prompt("Enter your age ");
 
-if (Number(age) === 18) {
-    alert("Congratulations! Enjoy your first year of riding");
-} else if (Number(age) > 18) {
-    alert("Enjoy your rides as you drive");
+    if (Number(age) === 18) {
+        alert("Congratulations! Enjoy your first year of riding");
+    } else if (Number(age) > 18) {
+        alert("Enjoy your rides as you drive");
 
-} else {
-    alert("Error! You are too young to drive. Power off")
+    } else {
+        alert("Error! You are too young to drive. Power off")
+    }
 }
+
+checkDriverAge();
