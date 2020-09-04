6b878934e07000cfc59e72a1e49b3b8178c55621
Asonglefac Millenium Nzefe <asonglefacmillenium@gmail.com>
2020-09-04 14:58:13 +0100

    Using both function expression and function declaration methods to perform the algorithm

--- script code/condition.js
+++ script code/condition.js
@@ -1,3 +1,4 @@
+/* Using function declaration */
 function checkDriverAge() {
     var age = prompt("Enter your age ");
 
@@ -12,3 +13,22 @@ function checkDriverAge() {
 }
 
 checkDriverAge();
+
+
+/*Using function Expresion*/
+
+var checkDriverAge2 = function() {
+    var age = prompt("Enter your age ");
+
+    if (Number(age) === 18) {
+        alert("Congratulations! Enjoy your first year of riding");
+    } else if (Number(age) > 18) {
+        alert("Enjoy your rides as you drive");
+
+    } else {
+        alert("Error! You are too young to drive. Power off")
+    }
+}
+
+
+checkDriverAge2();
